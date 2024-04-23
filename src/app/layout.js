b4801fa5ac30544def63cb4f7306f3ebb722f2c7
love singh kalbhor <lovesinghkalbhor@gmail.com>
2024-04-23 16:31:21 +0530

    initial commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import "@/global-css/home.css";
+import Navbar from "@/custom_components/navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +12,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Navbar></Navbar>
+
+        {children}
+      </body>
     </html>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/custom_components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
@@ -13,6 +13,13 @@ module.exports = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    screens: {
+      xs: "414px",
+      sm: "640px	",
+      md: "768px	",
+      lg: "1024px",
+      xl: "1280px",
+    },
   },
-  plugins: [],
+  plugins: [require("daisyui")],
 };
