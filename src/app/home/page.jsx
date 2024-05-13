216b2c3b8f0d52cb9640dcdf55a6c987056669b4
love singh kalbhor <lovesinghkalbhor@gmail.com>
2024-05-13 19:13:55 +0530

    first commit

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,9 +1,90 @@
+"use client";
 import Image from "next/image";
+import { motion } from "framer-motion";
+import { useRef, useEffect, useState } from "react";
+import { stagger } from "framer-motion";
+const about_us_heading = "ABOUT US";
+const our_services_heading = "OUR SERVICES";
+const hero_heading = "WE ARE GROWTHIFY SERVICES ONLINE BUSINESS SOLUTION";
+const our_client_heading = "OUR CLIENT";
+const hero_para =
+  "We help your business to grow online with minimal efforts and helops you to grow your business";
 
 export default function Home() {
+  // animation for cards
+  const card_animation_parent = {
+    hidden: { opacity: 0, translateX: -100 },
+    show: {
+      opacity: [0, 1],
+      translateX: 0,
+      transition: {
+        duration: 0.6,
+        type: "spring",
+        // delay: 0.4,
+        bounce: 0.3,
+      },
+    },
+  };
+  const text_reveal = {
+    hidden: { opacity: 0 },
+    show: {
+      opacity: [0, 1],
+    },
+  };
+  function stringToCharArray(str) {
+    // Use the spread operator (...) to unpack the string into an array of characters
+    return [...str];
+  }
+  // animation for first moving stripe
+  const [isHovering, setIsHovering] = useState(false); // Initial state is not hovering
+  const contentRef = useRef(null);
+  const containerWidth = useRef(0);
+  const animationProgress = useRef(0);
+
+  useEffect(() => {
+    if (contentRef.current) {
+      containerWidth.current = contentRef.current.offsetWidth;
+      animationProgress.current = containerWidth.current * -1; // Initial progress
+    }
+  }, [contentRef]);
+  useEffect(() => {
+    setIsHovering(true);
+  }, []);
+
+  const handleHoverStart = () => {
+    setIsHovering(false);
+  };
+
+  const handleHoverEnd = () => {
+    setIsHovering(true);
+  };
+
+  // animation for protfolio moving stipe
+  const [isHoveringportfolio, setIsHoveringportfolio] = useState(false); // Initial state is not hovering
+  const contentRefportfolio = useRef(null);
+  const containerWidthportfolio = useRef(0);
+  const animationProgressportfolio = useRef(0);
+
+  useEffect(() => {
+    if (contentRefportfolio.current) {
+      containerWidthportfolio.current = contentRefportfolio.current.offsetWidth;
+      animationProgressportfolio.current = containerWidthportfolio.current * -1; // Initial progress
+    }
+  }, [contentRefportfolio]);
+  useEffect(() => {
+    setIsHoveringportfolio(true);
+  }, []);
+
+  const handleHoverStartportfolio = () => {
+    setIsHoveringportfolio(false);
+  };
+
+  const handleHoverEndportfolio = () => {
+    setIsHoveringportfolio(true);
+  };
   return (
     <>
-      <div className=" hero__container overflow-x-hidden relative">
+      <div className=" hero__container overflow-x-hidden relative sm:mb-40 xs:mb-20">
         <Image
           className="object-cover absolute h-full w-full"
           src="/images/hero-workers-background-image.png"
@@ -16,13 +97,31 @@ export default function Home() {
           <div className="lg:col-span-3 md:col-span-4  sm:my-44 my-28 md:mx-16  sm:mx-10  mx-6 ">
             {/* <div className="grid-cols-3 my-44 mx-20 max-w-2xl"> */}
             <div className="hero__text__container col-span-6 row-start-1 col-start-2 sm:mt-0 mt-40">
-              <h1 className="xl:text-[3.3rem] lg:text-[3rem] md:text-4xl sm:text-4xl xs:3xl text-2xl  lg:leading-tight  font-bold">
-                WE ARE GROWTHIFY SERVICES ONLINE BUSINESS SOLUTION
-              </h1>
-              <p className="lg:text-xl md:text-lg sm:text-base text-sm mt-4 pr-12">
-                We help your business to grow online with minimal efforts and
-                helops you to grow your business{" "}
-              </p>
+              <motion.h1
+                className="xl:text-[2.9rem] lg:text-[2.9rem] md:text-3xl sm:text-4xl xs:3xl text-2xl  lg:leading-tight  font-bold"
+                initial="hidden"
+                whileInView="show"
+                transition={{ staggerChildren: 0.02 }}
+              >
+                {stringToCharArray(hero_heading).map((char) => (
+                  <motion.span key={char} variants={text_reveal}>
+                    {char}
+                  </motion.span>
+                ))}
+              </motion.h1>
+
+              <motion.p
+                initial="hidden"
+                whileInView="show"
+                transition={{ staggerChildren: 0.01 }}
+                className=" sm:text-base text-sm mt-4 pr-12"
+              >
+                {stringToCharArray(hero_para).map((char) => (
+                  <motion.span key={char} variants={text_reveal}>
+                    {char}
+                  </motion.span>
+                ))}
+              </motion.p>
             </div>
 
             <div className="hero__button__container col-span-6 row-start-3 col-start-2">
@@ -49,17 +148,64 @@ export default function Home() {
           ></Image>
         </header>
       </div>
-      <section className="moving__strip overflow-hidden md:-mt-20 sm:-mt-16 xs:-mt-20">
-        <span>DIGITAL MARKETING</span>
-        <span>UI/UX</span>
-        <span>DIGITAL MARKETING</span>
-        <span>DIGITAL MARKETING</span>
-        <span>DIGITAL MARKETING</span>
-        <span>DIGITAL MARKETING</span>
-        <span>DIGITAL MARKETING</span>
-      </section>
 
-      <section className="relative md:mt-56 sm:mt-48 xs:mt-32 mt-20">
+      <motion.div
+        ref={contentRef}
+        className="relative overflow-hidden md:-mt-20 sm:-mt-16 xs:-mt-20"
+        onMouseEnter={handleHoverStart}
+        onMouseLeave={handleHoverEnd}
+      >
+        <div className="moving__strip_left"></div>
+        <div className="moving__strip_right"></div>
+        <motion.div
+          className="space-x-16 cursor-pointer moving__strip  "
+          animate={{
+            x: isHovering
+              ? animationProgress.current
+              : containerWidth.current * -1, // Pause at current position on hover
+            // Pause at current position
+            transition: { duration: 5, repeat: Infinity, ease: "linear" }, // Customize as needed
+          }}
+        >
+          <span>FRONTEND DEVELOPMENT</span>
+          <span>WEB DEVELOPMENT</span>
+          <span>BACKEND DEVELOPMENT</span>
+          <span>E-COMMERSE WEBSITE</span>
+          <span>FRONTEND DEVELOPMENT</span>
+          <span>WEB DEVELOPMENT</span>
+          <span>BACKEND DEVELOPMENT</span>
+          <span>E-COMMERSE WEBSITE</span>
+          <span>FRONTEND DEVELOPMENT</span>
+          <span>WEB DEVELOPMENT</span>
+          <span>BACKEND DEVELOPMENT</span>
+          <span>E-COMMERSE WEBSITE</span>
+        </motion.div>
+        {/* <motion.div
+          className="space-x-16 cursor-pointer moving__strip"
+          animate={{
+            x: isHovering ? animationProgress.current : containerWidth.current, // Pause at current position on hover
+            transition: { duration: 5, repeat: Infinity, ease: "linear" }, // Customize as needed
+          }}
+        >
+          <span>DIGITAL MARKETING</span>
+          <span>SOCIAL MEDIA</span>
+          <span>GOOGLE AD</span>
+          <span>SOCIAL MEDIA AD</span>
+          <span>INSTAGRAM MARKETING</span>
+          <span>DIGITAL MARKETING</span>
+          <span>SOCIAL MEDIA</span>
+          <span>GOOGLE AD</span>
+          <span>SOCIAL MEDIA AD</span>
+          <span>INSTAGRAM MARKETING</span>
+          <span>DIGITAL MARKETING</span>
+          <span>SOCIAL MEDIA</span>
+          <span>GOOGLE AD</span>
+          <span>SOCIAL MEDIA AD</span>
+          <span>INSTAGRAM MARKETING</span>
+        </motion.div> */}
+      </motion.div>
+
+      <section className="relative md:mt-40 sm:mt-48 xs:mt-32 mt-20">
         <div className="about__us xl:container  xl:mx-auto flex lg:flex-row-reverse   pb-60">
           <div className="about__us-image-container relative">
             <Image
@@ -71,7 +217,21 @@ export default function Home() {
           <div className="lg:w-1/2 z-50   mb-32">
             <div>
               <div className="section__heading justify-start lg:py-28 md:py-24 sm:py-20 pt-20 pb-32 md:px-10 px-0">
-                ABOUT US
+                <motion.h1
+                  initial="hidden"
+                  whileInView="show"
+                  transition={{ staggerChildren: 0.1 }}
+                >
+                  {stringToCharArray(about_us_heading).map((char) => (
+                    <motion.span
+                      key={char}
+                      variants={text_reveal}
+                      transition={{ duration: 0.2 }}
+                    >
+                      {char}
+                    </motion.span>
+                  ))}
+                </motion.h1>
                 <hr />
               </div>
             </div>
@@ -116,7 +276,7 @@ export default function Home() {
       </section>
 
       <div className=" 2xl:container relative 2xl:mx-auto ">
-        <section className="our__services pb-20">
+        <section className="our__services pb-20 sm:mb-60 mb-20">
           <div className=" xl:container relative xl:mx-auto 2xl:px-20">
             {/* <div>
             <Image
@@ -130,11 +290,31 @@ export default function Home() {
 
             <div>
               <div className="section__heading w-full justify-start py-28 sm:px-10 px-6">
-                OUR SERVICES
+                <motion.h1
+                  initial="hidden"
+                  whileInView="show"
+                  transition={{ staggerChildren: 0.1 }}
+                >
+                  {stringToCharArray(our_services_heading).map((char) => (
+                    <motion.span
+                      key={char}
+                      variants={text_reveal}
+                      transition={{ duration: 0.2 }}
+                    >
+                      {char}
+                    </motion.span>
+                  ))}
+                </motion.h1>
                 <hr />
               </div>
               {/* <div className="our__services-cards-container grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 xs:grid-cols-2 grid-cols-1 gap-10 px-10"> */}
-              <div className="our__services-cards-container  sm:px-10 px-6">
+              <motion.div
+                initial="hidden"
+                whileInView="show"
+                variants={card_animation_parent}
+                viewport={{ amount: 0.4 }}
+                className="our__services-cards-container  sm:px-10 px-6"
+              >
                 <div className="our__services-cards relative z-10">
                   <Image
                     src="/images/digital_marketing.png"
@@ -165,21 +345,48 @@ export default function Home() {
                     </p>
                   </div>
                 </div>
-              </div>
+              </motion.div>
             </div>
           </div>
         </section>
       </div>
-      <section className="portfolio__strip overflow-hidden sm:mt-10 mt-6">
-        <span>OUR</span>
-        <span>PORTFOLIO</span>
-        <span>OUR</span>
-        <span>DIGITAL MARKETING</span>
-        <span>WEB</span>
-        <span>DEVELOPMENT</span>
-      </section>
-      <section className="portfolio mt-20 container mx-auto mb-28">
-        <div className="grid md:grid-cols-3 xs:grid-cols-2 gap-5 xs:p-0 p-10">
+
+      <motion.div
+        ref={contentRefportfolio}
+        className="relative overflow-hidden "
+        onMouseEnter={handleHoverStartportfolio}
+        onMouseLeave={handleHoverEndportfolio}
+      >
+        <div className="moving__strip_left"></div>
+        <div className="moving__strip_right"></div>
+        <motion.div
+          className="portfolio__strip"
+          animate={{
+            x: isHoveringportfolio ? animationProgressportfolio.current : 0, // Pause at current position on hover
+            // Pause at current position
+            transition: { duration: 10, repeat: Infinity, ease: "linear" }, // Customize as needed
+          }}
+        >
+          <span>OUR PORTFOLIO</span>
+          <span>DIGITAL MARKETING</span>
+          <span>WEB DEVELOPMENT</span>
+          <span>OUR PORTFOLIO</span>
+          <span>DIGITAL MARKETING</span>
+          <span>OUR PORTFOLIO</span>
+          <span>DIGITAL MARKETING</span>
+          <span>WEB DEVELOPMENT</span>
+          <span>OUR PORTFOLIO</span>
+          <span>DIGITAL MARKETING</span>
+          <span>OUR PORTFOLIO</span>
+          <span>DIGITAL MARKETING</span>
+          <span>WEB DEVELOPMENT</span>
+          <span>OUR PORTFOLIO</span>
+          <span>DIGITAL MARKETING</span>
+        </motion.div>
+      </motion.div>
+
+      <section className="portfolio mt-20 container mx-auto mb-28 md:p-12 p-4">
+        <div className="grid md:grid-cols-3 xs:grid-cols-2 gap-16 xs:p-0 p-16 ">
           <div className="space-y-10">
             <div className="portfolio-image-container">
               <Image
@@ -323,11 +530,31 @@ export default function Home() {
       <div className=" bg-slate-950">
         <section className="our__clients xl:container mx-auto">
           <div className="section__heading w-full justify-start py-36 px-10">
-            OUR CLIENTS
+            <motion.h1
+              initial="hidden"
+              whileInView="show"
+              transition={{ staggerChildren: 0.1 }}
+            >
+              {stringToCharArray(our_client_heading).map((char) => (
+                <motion.span
+                  key={char}
+                  variants={text_reveal}
+                  transition={{ duration: 0.2 }}
+                >
+                  {char}
+                </motion.span>
+              ))}
+            </motion.h1>
             <hr />
           </div>
 
-          <div className="relative  grid sm:grid-cols-2 gap-5 p-20 px-10 pt-5 lg:w-3/5 md:w-4/5 justify-items-end">
+          <motion.div
+            initial="hidden"
+            whileInView="show"
+            variants={card_animation_parent}
+            viewport={{ amount: 0.2 }}
+            className="relative  grid sm:grid-cols-2 gap-5 p-20 px-10 pt-5 lg:w-3/5 md:w-4/5 justify-items-end"
+          >
             <Image
               className="absolute our__clients-background-light -top-0"
               src="/images/background-light.png"
@@ -483,7 +710,7 @@ export default function Home() {
                 </p>
               </div>
             </div>
-          </div>
+          </motion.div>
         </section>
       </div>
 
